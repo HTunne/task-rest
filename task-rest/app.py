06e9f43f101ecd72bc7e25f9f5936e2c5edef545
HTunne <py13ht@leeds.ac.uk>
2020-09-16 20:28:16 +0100

    Update the pipfile.

--- task-rest/app.py
+++ task-rest/app.py
@@ -1,13 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
-from flask_marshmallow import Marshmallow
 
 from resources import TaskResource, TaskListResource, TaskCommandResource
 
 app = Flask(__name__)
 api = Api(app)
-ma = Marshmallow(app)
 
 CORS(app, resources={r'/*': {'origins': '*'}})
 api.add_resource(TaskListResource, '/')
