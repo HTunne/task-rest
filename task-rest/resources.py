01cb1c55e2d9ed0e09b9e438ed32a76a9a6250eb
HTunne <py13ht@leeds.ac.uk>
2020-09-22 02:00:20 +0100

    Add messages to reponses that confirm changes and expose errors.

--- task-rest/resources.py
+++ task-rest/resources.py
@@ -3,6 +3,7 @@ from flask import current_app
 from flask import jsonify, request, make_response
 from flask_restful import Resource
 from tasklib import TaskWarrior, Task
+from tasklib.backends import TaskWarriorException
 from werkzeug.security import check_password_hash
 from datetime import datetime, timedelta
 import jwt
@@ -28,30 +29,54 @@ def token_required(f):
         return f(*args, **kwargs)
     return decorator
 
+def expose_errors(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        try:
+            result = f(*args, **kwargs)
+        except TaskWarriorException as e:
+            error = '\n'.join([x for x in str(e).split('\n') if not (x.startswith("Configuration override") or x.startswith("Command used"))])
+            result = jsonify({'message': {
+                'description': error,
+                'type': 'error'
+            }})
+        return result
+    return decorator
+
+def json_task_and_message(task, message, msg_type):
+    return jsonify({
+        'task': ts.dump(task),
+        'message': {
+            'description': message.format(task['id']),
+            'type': msg_type
+        }
+    })
 
 
 class TaskResource(Resource):
-    method_decorators = [token_required]
+    method_decorators = [token_required, expose_errors]
     def get(self, task_uuid):
         task = tw.tasks.get(uuid = task_uuid)
         return jsonify({'task': ts.dump(task)})
 
+    # task modify
     def put(self, task_uuid):
         task = tw.tasks.get(uuid = task_uuid)
         data = ts.loads(request.data)
         for key in data:
             task[key] = data[key]
         task.save()
-        return jsonify({'task': ts.dump(task)})
+        return json_task_and_message(task, 'Modified task {}.', 'success')
 
+    # task delete
     def delete(self, task_uuid):
         task = tw.tasks.get(uuid = task_uuid)
         task.delete()
-        return jsonify({'task': ts.dump(task)})
+        return json_task_and_message(task, 'Deleted task {}.', 'info')
 
 
 class TaskListResource(Resource):
-    method_decorators = [token_required]
+    method_decorators = [token_required, expose_errors]
     def get(self):
         return jsonify({'tasks': ts.dump(tw.tasks.all(), many=True)})
 
@@ -61,36 +86,43 @@ class TaskListResource(Resource):
         for key in data:
             task[key] = data[key]
         task.save()
-        return jsonify({'task': ts.dump(task)})
+        return json_task_and_message(task, 'Added task {}.', 'success')
 
 
 class TaskCommandResource(Resource):
-    method_decorators = [token_required]
+    method_decorators = [token_required, expose_errors]
     def put(self, task_uuid, command):
         task = tw.tasks.get(uuid = task_uuid)
         if command == 'done':
+            msg = 'Completed task {}.'
             task.done()
         elif command == 'start':
+            msg = 'Started task {}.'
             task.start()
         elif command == 'stop':
+            msg = 'Stopped task {}.'
             task.stop()
         elif command == 'add_annotation':
+            msg = 'Added annotation to task {}.'
             data = tas.loads(request.data)
             task.add_annotation(data["description"])
         elif command == 'remove_annotation':
+            msg = 'Removed annotaion from task {}'
             data = tas.loads(request.data)
             task.remove_annotation(data["description"])
         elif command == 'add_dependency':
+            msg = 'Added dependency to task {}'
             data = dts.loads(request.data)
             dep = tw.tasks.get(uuid = data['uuid'])
             task['depends'].add(dep)
             task.save()
         elif command == 'remove_dependency':
+            msg = 'Removed dependency to task {}'
             data = dts.loads(request.data)
             dep = tw.tasks.get(uuid = data['uuid'])
             task['depends'].remove(dep)
             task.save()
-        return jsonify({'task': ts.dump(task)})
+        return json_task_and_message(task, msg, 'info')
 
 
 class AuthResource(Resource):
